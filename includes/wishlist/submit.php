85b3f8d9d1476c820c02795800dd40cc46ee6766
Nathan Onn <nathan@smashingadvantage.com>
2023-06-01 02:45:01 +0000

    updates

---
+++ includes/wishlist/submit.php
@@ -0,0 +1,9 @@
+<?php
+
+    if ( class_exists( 'Products' ) ) 
+        $products = new Products();
+
+    // instructions: if the form was submitted, add the product to the wishlist or remove it from the wishlist (if it's already in the wishlist)
+
+
+
